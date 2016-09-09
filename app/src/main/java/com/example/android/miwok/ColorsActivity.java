27b0f424939cd7abc2cb27fe7777f18b392c9982
farhanarnob <farhanarnob@live.com>
2016-09-09 14:54:04 +0600

    transform variables, methods from colorsActivity to ColorsFragment

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,115 +1,24 @@
 package com.example.android.miwok;
 
-import android.content.Context;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
-import java.util.ArrayList;
+import android.support.v7.app.AppCompatActivity;
 
 public class ColorsActivity extends AppCompatActivity {
-    private MediaPlayer mediaPlayer;
-    AudioManager audioManager;
 
-    //variable for audio focus gain or not
-    private int result;
-
-    /**
-     * Creating audioFocusChangListener so that we can take action to control what is be change on media player if other audio will play
-     */
-    private AudioManager.OnAudioFocusChangeListener audioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
-        @Override
-        public void onAudioFocusChange(int focusChange) {
-            if(focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT
-                    || focusChange == AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK){
-                mediaPlayer.pause();
-                mediaPlayer.seekTo(0);
-            }
-            else if (focusChange == AudioManager.AUDIOFOCUS_GAIN){
-                mediaPlayer.start();
-            }
-            else if(focusChange == AudioManager.AUDIOFOCUS_LOSS){
-                releasedMediaPlayer();
-            }
-        }
-    };
-    private MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mp) {
-            releasedMediaPlayer();
-        }
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
+        setContentView(R.layout.activity_category);
         //creating up navigation action
-
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new ColorsFragment()).commit();
         //Array list for color word
-        final ArrayList<Word> colors = new ArrayList<>();
-        colors.add(new Word("red","weṭeṭṭi",R.drawable.color_red,R.raw.color_red));
-        colors.add(new Word("green","chokokki",R.drawable.color_green,R.raw.color_green));
-        colors.add(new Word("brown","ṭakaakki",R.drawable.color_brown,R.raw.color_brown));
-        colors.add(new Word("gray","ṭopoppi",R.drawable.color_gray,R.raw.color_gray));
-        colors.add(new Word("black","kululli",R.drawable.color_black,R.raw.color_black));
-        colors.add(new Word("white","kelelli",R.drawable.color_white,R.raw.color_white));
-        colors.add(new Word("dusty yellow","ṭopiisә",R.drawable.color_dusty_yellow,R.raw.color_dusty_yellow));
-        colors.add(new Word("mustard yellow","chiwiiṭә",R.drawable.color_mustard_yellow,R.raw.color_mustard_yellow));
 
-        // initializing audio manager
-        audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-
-        //Request audio focus for playback
-        result = audioManager.requestAudioFocus(audioFocusChangeListener,AudioManager.STREAM_MUSIC,AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-
-        //Word Adapter for displaying color
-        WordAdapter adapter = new WordAdapter(this,colors,R.color.category_colors);
-        final ListView colorList = (ListView) findViewById(R.id.list);
-        colorList.setAdapter(adapter);
-        colorList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Word color = colors.get(position);
-                mediaPlayer = MediaPlayer.create(ColorsActivity.this,color.getmSoundResourceID());
-                mediaPlayer.start();
-                colorList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Word word = colors.get(position);
-                        // Release the media player if it currently exists because we are about to play
-                        // a different sound file.
-                        releasedMediaPlayer();
-                        mediaPlayer = MediaPlayer.create(ColorsActivity.this,word.getmSoundResourceID());
-
-                        if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED){
-                            Log.v("ColorsActivity","start");
-                            mediaPlayer.start();
-                        }
-                        // Setup a listener on the media player, so that we can stop and release the
-                        // media player once finished the playing.
-                        mediaPlayer.setOnCompletionListener(onCompletionListener);
-
-                    }
-                });
-            }
-        });
     }
-    private void releasedMediaPlayer() {
-        if (mediaPlayer != null) {
-            mediaPlayer.release();
-            mediaPlayer = null;
-        }
-            audioManager.abandonAudioFocus(audioFocusChangeListener);
-    }
+
     @Override
     protected void onStop() {
         super.onStop();
         //
-        releasedMediaPlayer();
+
     }
 }
--- app/src/main/java/com/example/android/miwok/ColorsFragment.java
+++ app/src/main/java/com/example/android/miwok/ColorsFragment.java
@@ -1,18 +1,52 @@
 package com.example.android.miwok;
 
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ColorsFragment extends Fragment {
+    AudioManager audioManager;
+    private MediaPlayer mediaPlayer;
+    //variable for audio focus gain or not
+    private int result;
 
+    /**
+     * Creating audioFocusChangListener so that we can take action to control what is be change on media player if other audio will play
+     */
+    private AudioManager.OnAudioFocusChangeListener audioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT
+                    || focusChange == AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK) {
+                mediaPlayer.pause();
+                mediaPlayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                mediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                releasedMediaPlayer();
+            }
+        }
+    };
+    private MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releasedMediaPlayer();
+        }
+    };
 
     public ColorsFragment() {
         // Required empty public constructor
@@ -22,9 +56,70 @@ public class ColorsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
+        final ArrayList<Word> colors = new ArrayList<>();
+        colors.add(new Word("red", "weṭeṭṭi", R.drawable.color_red, R.raw.color_red));
+        colors.add(new Word("green", "chokokki", R.drawable.color_green, R.raw.color_green));
+        colors.add(new Word("brown", "ṭakaakki", R.drawable.color_brown, R.raw.color_brown));
+        colors.add(new Word("gray", "ṭopoppi", R.drawable.color_gray, R.raw.color_gray));
+        colors.add(new Word("black", "kululli", R.drawable.color_black, R.raw.color_black));
+        colors.add(new Word("white", "kelelli", R.drawable.color_white, R.raw.color_white));
+        colors.add(new Word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        colors.add(new Word("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
+
+        // initializing audio manager
+        audioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+
+        //Request audio focus for playback
+        result = audioManager.requestAudioFocus(audioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+        //Word Adapter for displaying color
+        WordAdapter adapter = new WordAdapter(getActivity(), colors, R.color.category_colors);
+        final ListView colorList = (ListView) rootView.findViewById(R.id.list);
+        colorList.setAdapter(adapter);
+        colorList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Word color = colors.get(position);
+                mediaPlayer = MediaPlayer.create(getActivity(), color.getmSoundResourceID());
+                mediaPlayer.start();
+                colorList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Word word = colors.get(position);
+                        // Release the media player if it currently exists because we are about to play
+                        // a different sound file.
+                        releasedMediaPlayer();
+                        mediaPlayer = MediaPlayer.create(getActivity(), word.getmSoundResourceID());
+
+                        if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                            Log.v("ColorsActivity", "start");
+                            mediaPlayer.start();
+                        }
+                        // Setup a listener on the media player, so that we can stop and release the
+                        // media player once finished the playing.
+                        mediaPlayer.setOnCompletionListener(onCompletionListener);
+
+                    }
+                });
+            }
+        });
+
+        return rootView;
     }
 
+    private void releasedMediaPlayer() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        audioManager.abandonAudioFocus(audioFocusChangeListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        releasedMediaPlayer();
+    }
 }
